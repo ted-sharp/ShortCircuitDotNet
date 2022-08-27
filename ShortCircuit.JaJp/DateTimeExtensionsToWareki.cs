7e3738bd6a650cb38e0af08ca40ddbedf10b3623
ted.sharp <ted.sharp.flat@gmail.com>
2022-08-28 01:30:00 +0900

    拡張メソッドのメモを追加

---
+++ ShortCircuit.JaJp/DateTimeExtensionsToWareki.cs
@@ -0,0 +1,15 @@
+﻿namespace ShortCircuit.JaJp
+{
+    public static class DateTimeExtensionsToWareki
+    {
+        // 和暦
+        // 役所や病院は基本的に和暦
+        // 外部から注入してメンテできるようにしておく
+
+        // 干支
+        // カレンダーに使う？
+
+        // 六曜
+        // カレンダーに使う？
+    }
+}
